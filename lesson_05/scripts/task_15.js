93a8e7b9368c70a1434d36318c6ce6bc8c45bacc
PliutaNastya <koicheva1996@gmail.com>
2025-02-06 17:30:57 +0200

    add: task 15

--- lesson_05/scripts/task_15.js
+++ lesson_05/scripts/task_15.js
@@ -1,3 +1,16 @@
 "use strict"
 
 // Задача 15. Користувача поступово вводить показники температури протягом року. Знайти середню температуру.
+
+let sum = 0
+if (confirm('Почати тестування?')) {
+
+	for (let i = 1; i <= 12; i++) {
+		const userAnswer = parseInt(prompt(`Введіть температуру за ${i} місяць`, '21'))
+
+		sum += userAnswer
+	}
+
+	const averageTemperature = (sum / 12).toFixed(2)
+	document.write(`<p>Середня температура протягом року склала: ${averageTemperature}&#8451;</p>`)
+}
