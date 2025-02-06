59f3402e9213c34d453f2268059afe7fd5f84710
PliutaNastya <koicheva1996@gmail.com>
2025-02-06 17:23:58 +0200

    add: task 9

--- lesson_05/scripts/task_09.js
+++ lesson_05/scripts/task_09.js
@@ -1,3 +1,20 @@
 "use strict"
 
 // Задача 09. Користувач загадує число. Комп’ютер задає питання поки не вгадає число користувача
+
+if (confirm('Почати тестування?')) {
+	const userNumber = parseInt(prompt('Загадайте будь-яке число починаючи з 1', '15'))
+
+	document.write(`<p>Ви загадали число: ${userNumber}</p>`)
+
+	let attempt
+
+	do {
+		attempt = Math.floor(Math.random() * userNumber) + 1
+
+		confirm(`Ви це число загадали? ${attempt} `)
+
+		if (attempt === userNumber) break
+
+	} while (attempt !== userNumber)
+}
